8d75f6ca9b13f64f57ae5e49fd46c1162377d0d6
tillenotuoma <totuoma27@gmail.com>
2025-02-27 21:06:50 +0300

    done

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,10 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
     
-    def learn(self):
+    def learn(self, knowledge):
+        self.knowledge.append(knowledge)
         pass
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 from user import User
-
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Python Basics", "OOP", "Data Structures", "Algorithms"]  
 
     def teach(self):
-        pass
+        random_index = random.randint(0, len(self.knowledge) - 1)
+        return self.knowledge[random_index]
